cf4091cdeedcd2397328bb07f9be6f489ad14780
athena-ye <athenaye@college.harvard.edu>
2020-10-01 10:56:40 -0400

    fixed multipleruns bug

--- multipleruns.py
+++ multipleruns.py
@@ -2,9 +2,10 @@
 import matplotlib.pyplot as plt
 import random as rand
 import numpy as np
+import filledflies as ff
 
 
-def multipleruns(numberofruns, temperaturefrequencyarray, per, gain, initvariabilityarray, driftvariabilityarray, heritabilityarray):
+def multipleruns(numberofdays, numberofruns, temperaturefrequencyarray, per, gain, initvariabilityarray, driftvariabilityarray, heritabilityarray):
 #     plt.title ('Number of Flies per Day')
 #     plt.xlabel ('Days')
 #     plt.ylabel ('Flies')
@@ -12,6 +13,7 @@ def multipleruns(numberofruns, temperaturefrequencyarray, per, gain, initvariabi
     variabilityarray=np.zeros((numberofruns, numberofdays))
     driftarray=np.zeros((numberofruns, numberofdays))
     mean=np.zeros((numberofdays))
+    x=np.arange(0,numberofdays)
 
     fig=plt.figure(figsize=(6,6))
 #     fig.add_subplot(1,2,1)
@@ -19,7 +21,8 @@ def multipleruns(numberofruns, temperaturefrequencyarray, per, gain, initvariabi
     colorsi=['gray','green']
     for i in range(0,2):
 #         plt.subplot(2,100,(1,45))
-        [fliesmatrix,flyarray]=filledflies(10,100,4,tempdata,5,initvariabilityarray[i],driftvariabilityarray[i],per[i], gain[i])
+        tempdata=gain[i]*np.sin(x*2*np.pi/per[i]+182*2*np.pi)+15.5
+        [fliesmatrix,flyarray]=ff.filledflies(10,100,4,tempdata,5,initvariabilityarray[i],driftvariabilityarray[i],per[i], gain[i])
         for f in range(0, fliesmatrix.shape[0]):
             plt.plot (fliesmatrix[f,:,2], colors[i])
             plt.plot(np.nanmean(fliesmatrix[:,:,2],axis=0),colorsi[i])
@@ -30,11 +33,9 @@ def multipleruns(numberofruns, temperaturefrequencyarray, per, gain, initvariabi
 
     fig=plt.figure(figsize=(8,8))
     colors=['pink','orange']
-    x=np.arange(0,365)
     plt.subplot(2,100,(103,150))
     for o in range (0,2):
-        temparray=gain[o]*np.sin(x*2*np.pi/per[o]+182*2*np.pi)+15.5
-        plt.plot(x, temparray, colors[o])
+        plt.plot(x, tempdata, colors[o])
     plt.ylabel('Temperature')
     plt.axis('tight')
 
@@ -44,7 +45,7 @@ def multipleruns(numberofruns, temperaturefrequencyarray, per, gain, initvariabi
 #     plt.ylabel ('Flies')
 
     for i in range(numberofruns):
-        filledflies(10,100,4,tempdata,5,0,4,365,8.5)
+        ff.filledflies(10,100,4,tempdata,5,0,4,365,8.5)
 #         bigarray=np.full((1,numberofdays),flyarray)
 #         flyarray[0]=numberofflies
         plt.plot (flyarray, 'pink')
